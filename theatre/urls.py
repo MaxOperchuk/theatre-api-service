e45b6cc6a771415acf0765259828313e29a60e78
Maksym Operchuk <operlast529@gmail.com>
2024-03-27 13:23:24 +0200

    create view sets for all models and register them.

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,11 +1,25 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from theatre.views import TheatreHallViewSet
+from theatre.views import (
+    TheatreHallViewSet,
+    PerformanceViewSet,
+    ReservationViewSet,
+    TicketViewSet,
+    PlayViewSet,
+    ActorViewSet,
+    GenreViewSet,
+)
 
 
 router = routers.DefaultRouter()
 router.register("theatre_halls", TheatreHallViewSet)
+router.register("performances", PerformanceViewSet)
+router.register("reservations", ReservationViewSet)
+router.register("tickets", TicketViewSet)
+router.register("plays", PlayViewSet)
+router.register("actors", ActorViewSet)
+router.register("genres", GenreViewSet)
 
 
 urlpatterns = [path("", include(router.urls))]
--- theatre/views.py
+++ theatre/views.py
@@ -1,11 +1,56 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 
-from theatre.models import TheatreHall
-from theatre.serializers import TheatreHallSerializer
+from theatre.models import (
+    TheatreHall,
+    Genre,
+    Actor,
+    Play,
+    Performance,
+    Reservation,
+    Ticket
+)
+
+from theatre.serializers import (
+    TheatreHallSerializer,
+    GenreSerializer,
+    ActorSerializer,
+    PlaySerializer,
+    PerformanceSerializer,
+    ReservationSerializer,
+    TicketSerializer
+)
 
 
 class TheatreHallViewSet(viewsets.ModelViewSet):
     queryset = TheatreHall.objects.all()
     serializer_class = TheatreHallSerializer
 
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class PlayViewSet(viewsets.ModelViewSet):
+    queryset = Play.objects.all()
+    serializer_class = PlaySerializer
+
+
+class PerformanceViewSet(viewsets.ModelViewSet):
+    queryset = Performance.objects.all()
+    serializer_class = PerformanceSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
