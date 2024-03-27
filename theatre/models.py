a253ea8a6509ba80a5d49b46b649d10dd15c925c
Maksym Operchuk <operlast529@gmail.com>
2024-03-27 12:46:50 +0200

    add Play model to models.py

--- theatre/models.py
+++ theatre/models.py
@@ -7,6 +7,13 @@ class TheatreHall(models.Model):
     seats_in_row = models.IntegerField()
 
 
+class Play(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField("Actor")
+    genres = models.ManyToManyField("Genre")
+
+
 class Performance(models.Model):
-    play = models.ForeignKey("Play", on_delete=models.CASCADE)
+    play = models.ForeignKey(Play, on_delete=models.CASCADE)
     theatre_hall = models.ForeignKey(TheatreHall, on_delete=models.CASCADE)
