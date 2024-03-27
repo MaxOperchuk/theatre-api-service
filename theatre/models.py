04c61c4f08eb3be5255e4f5a6d75143820b4cbe2
Maksym Operchuk <operlast529@gmail.com>
2024-03-27 12:49:28 +0200

    add Genre model to models.py

--- theatre/models.py
+++ theatre/models.py
@@ -7,6 +7,10 @@ class TheatreHall(models.Model):
     seats_in_row = models.IntegerField()
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
