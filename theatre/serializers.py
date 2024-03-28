65ae73ffdff1559de9655de01a88ec422ca96b65
Maksym Operchuk <operlast529@gmail.com>
2024-03-28 10:29:51 +0200

    delete theatre_hall_capacity field for PerformanceListSerializer

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -62,9 +62,6 @@ class PerformanceListSerializer(serializers.ModelSerializer):
     theatre_hall_name = serializers.CharField(
         source="theatre_hall.name", read_only=True
     )
-    theatre_hall_capacity = serializers.IntegerField(
-        source="theatre_hall.capacity", read_only=True
-    )
     tickets_available = serializers.IntegerField(read_only=True)
 
     class Meta:
@@ -74,7 +71,6 @@ class PerformanceListSerializer(serializers.ModelSerializer):
             "show_time",
             "play_title",
             "theatre_hall_name",
-            "theatre_hall_capacity",
             "tickets_available"
         )
 
